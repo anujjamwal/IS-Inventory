0aabc7480af72c373a7d5dab050106bc4893575d
pulkitkochar <pulkitko@thoughtworks.com>
2013-04-22 18:39:05 +0530

    wrote crud and migration of database

--- src/main/java/com/thoughtworks/is/controllers/Main.java
+++ src/main/java/com/thoughtworks/is/controllers/Main.java
@@ -1,116 +1,115 @@
-//package com.thoughtworks.is.controllers;
-//
-//import java.sql.Date;
-//import java.util.List;
-//
-//import org.hibernate.Session;
-//import org.hibernate.SessionFactory;
-//
-///**
-// * Created with IntelliJ IDEA.
-// * User: pulkitko
-// * Date: 4/18/13
-// * Time: 12:11 PM
-// * To change this template use File | Settings | File Templates.
-// */
-//public class Main {
-//
-//
-//    public static void main(String[] args) {
-//
-//
-//        // Read
-//        System.out.println("******* READ *******");
-//        List employees = list();
-//        System.out.println("Total Employees: " + employees.size());
-//
-//
-//        // Write
-//        System.out.println("******* WRITE *******");
-//        Employee empl = new Employee("Jack", "Bauer", new Date(System.currentTimeMillis()), "911");
-//        empl = save(empl);
-//        empl = read(empl.getId());
-//        System.out.printf("%d %s %s \n", empl.getId(), empl.getFirstname(), empl.getLastname());
-//
-//
-//
-//        // Update
-//        System.out.println("******* UPDATE *******");
-//        Employee empl2 = read(1l); // read employee with id 1
-//        System.out.println("Name Before Update:" + empl2.getFirstname());
-//        empl2.setFirstname("James");
-//        update(empl2);  // save the updated employee details
-//
-//        empl2 = read(1l); // read again employee with id 1
-//        System.out.println("Name Aftere Update:" + empl2.getFirstname());
-//
-//
-//        // Delete
-//        System.out.println("******* DELETE *******");
-//        delete(empl);
-//        Employee empl3 = read(empl.getId());
-//        System.out.println("Object:" + empl3);
-//    }
-//
-//
-//
-//    private static List list() {
-//        SessionFactory sf = HibernateUtil.getSessionFactory();
-//        Session session = sf.openSession();
-//
-//        List employees = session.createQuery("from Employee").list();
-//        session.close();
-//        return employees;
-//    }
-//    private static Employee read(Long id) {
-//        SessionFactory sf = HibernateUtil.getSessionFactory();
-//        Session session = sf.openSession();
-//
-//        Employee employee = (Employee) session.get(Employee.class, id);
-//        session.close();
-//        return employee;
-//    }
-//    private static Employee save(Employee employee) {
-//        SessionFactory sf = HibernateUtil.getSessionFactory();
-//        Session session = sf.openSession();
-//
-//        session.beginTransaction();
-//
-//        Long id = (Long) session.save(employee);
-//        employee.setId(id);
-//
-//        session.getTransaction().commit();
-//
-//        session.close();
-//
-//        return employee;
-//    }
-//
-//    private static Employee update(Employee employee) {
-//        SessionFactory sf = HibernateUtil.getSessionFactory();
-//        Session session = sf.openSession();
-//
-//        session.beginTransaction();
-//
-//        session.merge(employee);
-//
-//        session.getTransaction().commit();
-//
-//        session.close();
-//        return employee;
-//
-//    }
-//
-//    private static void delete(Employee employee) {
-//        SessionFactory sf = HibernateUtil.getSessionFactory();
-//        Session session = sf.openSession();
-//
-//        session.beginTransaction();
-//
-//        session.delete(employee);
-//
-//        session.getTransaction().commit();
-//
-//        session.close();
-//    }
-//}
+package com.thoughtworks.is.controllers;
+
+import java.sql.Date;
+import java.util.List;
+
+import com.thoughtworks.is.services.Asset;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+/**
+* Created with IntelliJ IDEA.
+* User: pulkitko
+* Date: 4/18/13
+* Time: 12:11 PM
+* To change this template use File | Settings | File Templates.
+*/
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+        // Read
+        System.out.println("******* READ *******");
+        List assets = getAll();
+        System.out.println("Total Assets: " + assets.size());
+
+
+        // Write
+        System.out.println("******* WRITE *******");
+        Asset asset = new Asset(1l, "Laptop", "Apple", "Air", "1234567890", "4 yrs", "TW/IND/GGN/LT/1", "4GB");
+        asset = save(asset);
+        asset = getById(asset.getId());
+        System.out.printf("%d %s %s \n", asset.getId(), asset.getModel(), asset.getBrand());
+
+
+
+        // Update
+        System.out.println("******* UPDATE *******");
+        Asset asset1 = getById(1l); // read Asset with id 1
+        System.out.println("Name Before Update:" + asset1.getBrand());
+        asset1.setBrand("Dell");
+        update(asset1);  // save the updated asset details
+
+        asset1 = getById(1l); // read again asset with id 1
+        System.out.println("Name Aftere Update:" + asset1.getBrand());
+
+
+        // Delete
+        System.out.println("******* DELETE *******");
+        delete(asset);
+        Asset asset2 = getById(asset1.getId());
+        System.out.println("Object:" + asset2);
+    }
+
+
+
+    private static List getAll() {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.openSession();
+        List assets = session.createQuery("from ASSET").list();
+        session.close();
+        return assets;
+    }
+    private static Asset getById(Long id) {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.openSession();
+        Asset asset = (Asset) session.get(Asset.class, id);
+        session.close();
+        return asset;
+    }
+    private static Asset save(Asset asset) {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session   = sf.openSession();
+
+        session.beginTransaction();
+
+        Long id = (Long) session.save(asset);
+        asset.setId(id);
+
+        session.getTransaction().commit();
+
+        session.close();
+
+        return asset;
+    }
+
+    private static Asset update(Asset asset) {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.openSession();
+
+        session.beginTransaction();
+
+        session.merge(asset);
+
+        session.getTransaction().commit();
+
+        session.close();
+        return asset;
+
+    }
+
+    private static void delete(Asset asset) {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.openSession();
+
+        session.beginTransaction();
+
+        session.delete(asset);
+
+        session.getTransaction().commit();
+
+        session.close();
+    }
+}
--- src/main/java/com/thoughtworks/is/services/Asset.java
+++ src/main/java/com/thoughtworks/is/services/Asset.java
@@ -1,17 +1,43 @@
 package com.thoughtworks.is.services;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ASSET")
+@AllArgsConstructor
+@NoArgsConstructor
 public @Data class Asset {
 
-     private String type;
-     private String brand;
-     private String model;
-     private String serialNo;
-     private String warranty;
-     private String assetTag;
-     private String description;
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "brand")
+    private String brand;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "serialNo")
+    private String serialNo;
+
+    @Column(name = "warranty")
+    private String warranty;
+
+    @Column(name = "assetTag")
+    private String assetTag;
+
+    @Column(name = "description")
+    private String description;
+
 
 }
 
